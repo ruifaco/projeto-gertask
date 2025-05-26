783967129ff807be9dd053d5be677f28752de30b
Rui Facó <rui.faco@sou.inteli.edu.br>
2025-05-25 23:35:13 -0300

    Reestrutura inicialização, middlewares e roteamento

--- server.js
+++ server.js
@@ -1,40 +1,44 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path"); 
+const pool = require("./config/db"); 
+
 const app = express();
-const db = require("./config/db");
-const path = require("path");
+const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-db.connect()
-  .then(() => {
-    console.log("Conectado ao banco de dados PostgreSQL");
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.static(path.join(__dirname, "public")));
 
-    app.use(express.json());
+const apiRoutes = require('./routes/index');
+app.use('/api', apiRoutes);
 
-    const userRoutes = require("./routes/userRoutes");
-    app.use("/users", userRoutes);
+const frontendRoutes = require("./routes/frontRoutes");
+app.use("/", frontendRoutes);
+app.use((req, res, next) => {
+  res.status(404).render(path.join(__dirname, "views/pages/404"), { pageTitle: "Não Encontrado" }); 
+});
 
-    const frontendRoutes = require("./routes/frontRoutes");
-    app.use("/", frontendRoutes);
+app.use((err, req, res, next) => {
+  console.error("ERRO DETECTADO:", err.stack);
+  res.status(500).render(path.join(__dirname, "views/pages/500"), { pageTitle: "Erro no Servidor" }); 
 
-    // Middleware para lidar com erros de rota não encontrada
-    app.use((req, res, next) => {
-      res.status(404).send("Página não encontrada");
-    });
+});
 
-    // Middleware para lidar com erros internos do servidor
-    app.use((err, req, res, next) => {
-      console.error(err.stack);
-      res.status(500).send("Erro no servidor");
-    });
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados na inicialização:', err);
+  } else {
+    console.log('Conexão com o banco de dados PostgreSQL verificada com sucesso. Hora do DB:', res.rows[0].now);
 
-    const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
       console.log(`Servidor rodando na porta ${PORT}`);
+      console.log(`Páginas do frontend em http://localhost:${PORT}`);
+      console.log(`API em http://localhost:${PORT}/api`);
     });
-  })
-  .catch((err) => {
-    console.error("Erro ao conectar ao banco de dados:", err);
-  });
+  }
+});
+
