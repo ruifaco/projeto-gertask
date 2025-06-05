6502f748725334d9f40d44d279b3174971396a83
Rui Facó <rui.faco@sou.inteli.edu.br>
2025-06-04 21:57:08 -0300

    definição da rota

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,8 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const userController = require("../controllers/userController");
 
-router.get("/", userController.getAllUsers);
-router.get("/:id", userController.getUserById);
-router.post("/", userController.createUser);
-router.put("/:id", userController.updateUser);
-router.delete("/:id", userController.deleteUser);
+router.get('/usuarios', (req, res) => {
+  res.send('rota de usuários');
+});
 
 module.exports = router;
