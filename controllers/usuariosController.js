1b454af3fb712a44b6d9de9c3ae93aa661b3f73b
Rui Facó <rui.faco@sou.inteli.edu.br>
2025-06-05 18:49:51 -0300

    adicionar comentario

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,10 +1,9 @@
 const UsuarioModel = require('../models/userModel');
 
-// GET /usuarios
 const listarUsuarios = async (req, res) => {
   try {
-    const usuarios = await UsuarioModel.listarTodos();
-    res.json(usuarios);
+    const usuarios = await UsuarioModel.listarTodos(); // chama o model
+    res.json(usuarios); // responde com JSON
   } catch (error) {
     console.error('Erro ao listar usuários:', error);
     res.status(500).json({ erro: 'Erro interno no servidor' });
