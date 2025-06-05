aced9caf6ed1dd1652e7ef84f3c440149a3f682d
Rui Facó <rui.faco@sou.inteli.edu.br>
2025-06-04 22:00:54 -0300

    config app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-const usuariosRoutes = require('./routes/usuarios');
+const express = require('express');
+const app = express();
 
+// middleware de rotas
+const usuariosRoutes = require('./routes/usuarios');
 app.use('/', usuariosRoutes);
+
+// subir servidor
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+});
