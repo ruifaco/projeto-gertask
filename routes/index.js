2c9cde22b525fdae1b18e039224454219b9e3caf
Rui Facó <rui.faco@sou.inteli.edu.br>
2025-06-05 21:02:00 -0300

    deletar arquivos desnecessarios

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userRoutes = require('./userRoutes');
-const taskRoutes = require('./taskRoutes');
-router.use('/users', userRoutes);
-router.use('/tasks', taskRoutes);
-module.exports = router;
